29010824606d7d8bef9b488cc63c4d47d9859cad
kinglewis2606-ai <kinglewis2606@googlemail.com>
2026-08-02 20:47:43 +0100

    Add detectRooms function for room detectionAdd room detection module scaffold

---
+++ lib/floorDetection/detectRooms.ts
@@ -0,0 +1,9 @@
+import { WallLine } from "./detectWalls";
+
+export async function detectRooms(
+  walls: WallLine[]
+) {
+  console.log("Detecting rooms from", walls.length, "walls");
+
+  return [];
+}
